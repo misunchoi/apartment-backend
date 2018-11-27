b2314a7b3736442ddab3c9f044a8121a9b9fc340
Mi Sun Choi <choi.msc033@gmail.com>
2018-11-27 11:24:02 -0800

    tests Devise registration and sign-in flows

--- db/migrate/20181127192111_add_jti_matcher_to_users.rb
+++ db/migrate/20181127192111_add_jti_matcher_to_users.rb
@@ -1,4 +1,4 @@
-class AddJtlMatcherToUsers < ActiveRecord::Migration[5.2]
+class AddJtiMatcherToUsers < ActiveRecord::Migration[5.2]
   def change
     add_column :users, :jti, :string
     add_index :users, :jti, unique: true
